4fab0ef63082a27204313dd313f0d5e855c19f93
Mincong HUANG <mincong.h@gmail.com>
2018-10-04 21:29:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# maven-failsafe-plugin-demo [![Build Status][travis-img]][travis]
+
+Maven Failsafe Plugin demo
+
+[travis]: https://travis-ci.org/mincong-h/maven-failsafe-plugin-demo
+[travis-img]: https://travis-ci.org/mincong-h/maven-failsafe-plugin-demo.svg?branch=master
---
+++ junit5/src/test/java/demo/Junit5IT.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Mincong Huang
+ * @since 1.0
+ */
+public class Junit5IT {
+
+  @Test
+  public void testProperty() {
+    assertTrue(true);
+  }
+}
